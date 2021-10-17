28e2ee54ee9a6a68c54b2d8d9ceb761bccddf88a
FrequencyPhysics <86565124+FrequencyPhysics@users.noreply.github.com>
2021-10-17 17:20:33 +0100

    Create Qrcode.py
    Scan with your phone and you can see a message!

---
+++ Qrcode.py
@@ -0,0 +1,12 @@
+import qrcode
+
+data = 'Hi how are you'
+
+qr = qrcode.QRCode(version = 1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+img = qr.make_image(fill_color = 'blue', back_color = 'black')
+
+img.save('qrcode2.png')
