891a3051add0412dcf805d45ff96926ddad40d6e
FrequencyPhysics <86565124+FrequencyPhysics@users.noreply.github.com>
2021-10-31 16:28:50 +0000

    Create Swap characters.py

---
+++ Swap characters.py
@@ -0,0 +1,10 @@
+#A python program to swap two characters/words
+charA= 'Anushka'
+charB= 'Ademide'
+
+temp1= charA
+charA = charB
+charB = temp1
+
+print('charA after swapping: {}'.format(charA))
+print('charB after swapping: {}'.format(charB))
