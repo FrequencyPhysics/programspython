47ac5f8983c7320ff02a205c215606a04bff9734
FrequencyPhysics <86565124+FrequencyPhysics@users.noreply.github.com>
2021-10-31 16:29:39 +0000

    Create Swap strings.py

---
+++ Swap strings.py
@@ -0,0 +1,10 @@
+#A python program to swap two strings
+stringA = 'R'
+stringB = 'T'
+
+temp2=stringA
+stringA=stringB
+stringB=temp2
+
+print('stringA after swapping: {}'.format(stringA))
+print('stringB after swapping: {}'.format(stringB))
